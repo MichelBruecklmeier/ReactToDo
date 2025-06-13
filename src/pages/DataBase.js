df2982a5993f14f56f39e7608433a1777d7ab598
2cool4u <78418038+MichelBruecklmeier@users.noreply.github.com>
2025-06-13 14:03:20 -0400

    Update DataBase.js

--- src/pages/DataBase.js
+++ src/pages/DataBase.js
@@ -4,7 +4,7 @@ import {deleteDoc, query, doc,where, getFirestore , collection, getDocs, addDoc,
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-      apiKey: "AIzaSyAXng_UHks8e24rRVpf4Jsw0cM1orXmUmg",    authDomain: "todo-react-e07c8.firebaseapp.com",    projectId: "todo-react-e07c8",    storageBucket: "todo-react-e07c8.firebasestorage.app",    messagingSenderId: "1059241195878",    appId: "1:1059241195878:web:6cf328eef1b00f9a4230a7",    measurementId: "G-V0HDPY4ZNB"
+            //REMOVE KEY
 };
 
 // Initialize Firebase
