a17caea74ef126ccb1b950a400fc452fc43d2f96
Michel Bruecklmeier <michelbruecklmeier@aurora-schools.org>
2025-06-15 13:33:39 -0400

    Remove Config.js from repo and ignore it

--- src/pages/Config.js
+++
@@ -1,7 +0,0 @@
-
-
-export default function Config(){
-    return {
-      apiKey: "AIzaSyAXng_UHks8e24rRVpf4Jsw0cM1orXmUmg",    authDomain: "todo-react-e07c8.firebaseapp.com",    projectId: "todo-react-e07c8",    storageBucket: "todo-react-e07c8.firebasestorage.app",    messagingSenderId: "1059241195878",    appId: "1:1059241195878:web:6cf328eef1b00f9a4230a7",    measurementId: "G-V0HDPY4ZNB"
-};
-}
